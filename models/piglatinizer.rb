566f729b046e0ba59e3f4135fd308626ef379d78
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-21 02:48:53 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,24 +2,15 @@ class PigLatinizer
   attr_reader :phrase
 
   VOWELS = ['a', 'e', 'i', 'o', 'u']
-  TWO_LETTER_CLUSTERS = ['pl', 'th', 'pr', 'wh', 'sk']
-  THREE_LETTER_CLUSTERS = ['spr', 'str']
 
   def piglatinize(word)
     array = word.split(//)
     if VOWELS.include?(array[0].downcase)
       array.join << "way"
-    elsif TWO_LETTER_CLUSTERS.include?(word[0..1].downcase)
-      first = array.shift(2).join
-      array << first
-      array.join << "ay"
-    elsif THREE_LETTER_CLUSTERS.include?(word[0..2].downcase)
-      first = array.shift(3).join
-      array << first
-      array.join << "ay"
     else
-      first = array.shift
-      array << first
+      while !VOWELS.include?(array[0].downcase)
+        array.push(array.shift)
+      end
       array.join << "ay"
     end
   end
